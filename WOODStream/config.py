dffe3fccd8bdba5cf9febb19355d4cab3348426a
sᴠɴ <100134110+svnig7@users.noreply.github.com>
2025-06-24 15:37:20 +0530

    Update config.py

--- WOODStream/config.py
+++ WOODStream/config.py
@@ -7,18 +7,19 @@ class Telegram:
     API_ID = int(env.get("API_ID"))
     API_HASH = str(env.get("API_HASH"))
     BOT_TOKEN = str(env.get("BOT_TOKEN"))
-    OWNER_ID = int(env.get('OWNER_ID', '7978482443'))
+    OWNER_ID = int(env.get('OWNER_ID', '1234567'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     DATABASE_URL = str(env.get('DATABASE_URL'))
-    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
-    SESSION_NAME = str(env.get('SESSION_NAME', 'WOODStream'))
+    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "bots_7_bots"))
+    SESSION_NAME = str(env.get('SESSION_NAME', 'streambot'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
-    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
+    FORCE_SUB_LINK = str(env.get('FORCE_SUB_LINK'))
+    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', True)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
-    FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/fff4ea35f47dce9626c69.jpg")
-    START_PIC = env.get('START_PIC', "https://graph.org/file/fff4ea35f47dce9626c69.jpg")
-    VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
+    FILE_PIC = env.get('FILE_PIC', "https://raw.githubusercontent.com/svnig7/images/refs/heads/main/streambotl.png")
+    START_PIC = env.get('START_PIC', "https://raw.githubusercontent.com/svnig7/images/refs/heads/main/streambotl.png")
+    VERIFY_PIC = env.get('VERIFY_PIC', "https://raw.githubusercontent.com/svnig7/images/refs/heads/main/streambotl.png")
     MULTI_CLIENT = False
     FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
     ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
@@ -33,9 +34,4 @@ class Server:
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
     FQDN = str(env.get("FQDN", BIND_ADDRESS))
-    URL = "http{}://{}{}/".format(
-        "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
-    )
-
-
-
+    URL = "https://{}/".format(FQDN)
